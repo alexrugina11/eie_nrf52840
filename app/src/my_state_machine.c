57c8fa43d7ba1ccc75827cb215dc78f6d102a910
Alex Rugina <alex.rugina@ucalgary.ca>
2025-10-23 20:32:34 -0600

    completed exercise 1

--- app/src/my_state_machine.c
+++ app/src/my_state_machine.c
@@ -9,9 +9,9 @@
 /*---------------------------------------------------------
  * Function Prototypes
  *---------------------------------------------------------*/
-static void led_on_state_entry(void *o);
+static void led_on_state_exit(void *o);
 static enum smf_state_result led_on_state_run(void *o);
-static void led_off_state_entry(void *o);
+static void led_off_state_exit(void *o);
 static enum smf_state_result led_off_state_run(void *o);
 
 /*---------------------------------------------------------
@@ -32,8 +32,8 @@ typedef struct {
  * Local Variables
  *---------------------------------------------------------*/
 static const struct smf_state led_states[] = {
-    [LED_ON_STATE]  = SMF_CREATE_STATE(led_on_state_entry, led_on_state_run, NULL, NULL, NULL),
-    [LED_OFF_STATE] = SMF_CREATE_STATE(led_off_state_entry, led_off_state_run, NULL, NULL, NULL)
+    [LED_ON_STATE]  = SMF_CREATE_STATE(NULL, led_on_state_run, led_on_state_exit, NULL, NULL),
+    [LED_OFF_STATE] = SMF_CREATE_STATE(NULL, led_off_state_run, led_off_state_exit, NULL, NULL)
 };
 
 static led_state_object_t led_state_object;
@@ -47,7 +47,7 @@ int state_machine_run(void) {
     return smf_run_state(SMF_CTX(&led_state_object));
 }
 
-static void led_on_state_entry(void *o) {
+static void led_on_state_exit(void *o) {
     LED_set(LED0, LED_ON);
 }
 
@@ -62,7 +62,7 @@ static enum smf_state_result led_on_state_run(void *o) {
     return SMF_EVENT_HANDLED;
 }
 
-static void led_off_state_entry(void *o) {
+static void led_off_state_exit(void *o) {
     LED_set(LED0, LED_OFF);
 }
 
